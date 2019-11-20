b9b0650e6f230190b600a7b070fcf3bb864b13f4
gabi <gabi@gabi.mail.guest>
2019-11-20 09:41:40 +0200

    modified the pi serial code with the initial implementation

--- serial_implementation/pi.cpp
+++ serial_implementation/pi.cpp
@@ -1,34 +1,38 @@
-#include <iostream> 
-#include <time.h>
-#include <limits.h>
+
+#include <bits/stdc++.h> 
   
-#define INTERVAL 48000 
+#define INTERVAL 1000
+#include <limits.h> 
 using namespace std; 
-  
-int main() 
-{ 
-    long long interval, i; 
-    long double rand_x, rand_y, origin_distance, pi; 
-    long long circle_points = 0, square_points = 0; 
+
+double calculate_pi() {
+    unsigned long long  i; 
+    double  rand_x, rand_y, origin_dist, pi; 
+    unsigned long long circle_points = 0, square_points = 0; 
 
     srand(time(NULL)); 
-
-    for (i = 0; i < UINT_MAX; i++) { 
-        rand_x = double(rand() % (INTERVAL + 1)) / INTERVAL; 
-        rand_y = double(rand() % (INTERVAL + 1)) / INTERVAL; 
+        for (i = 0; i < UINT_MAX; i++) {  
+            rand_x = double(rand() % (INTERVAL + 1)) / INTERVAL; 
+            rand_y = double(rand() % (INTERVAL + 1)) / INTERVAL; 
+      
+            origin_dist = rand_x * rand_x + rand_y * rand_y; 
+      
+            if (origin_dist <= 1) 
+                circle_points++; 
+      
+          	 square_points++; 
+      
+            pi = double(4 * circle_points) / square_points; 
+        } 
+  return pi;
   
-        origin_distance = rand_x * rand_x + rand_y * rand_y; 
+}
   
-        if (origin_distance <= 1) {
-            circle_points++; 
-        }
-  
-        square_points++; 
-  
-        pi = double(4 * circle_points) / square_points; 
-    } 
-  
-    cout << "\nFinal Estimation of Pi = " << pi; 
-  
+int main() 
+{   
+    double pi;
+    
+    pi = calculate_pi();
+    cout << "Final Estimation of Pi = " << pi << endl;  
     return 0; 
 } 
